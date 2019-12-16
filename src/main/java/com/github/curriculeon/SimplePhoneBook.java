69827254c5beee0857478b3e292cef120c61f8f8
perscholas-lhunter <lhunter@perscholas.org>
2019-12-16 11:16:23 -0500

    update

---
+++ src/main/java/com/github/curriculeon/SimplePhoneBook.java
@@ -0,0 +1,32 @@
+package com.github.curriculeon;
+
+import java.util.List;
+
+/**
+ * Created by leon on 12/16/2019.
+ */
+public class SimplePhoneBook {
+    public void add(String name, String phoneNumberToAdd) {
+
+    }
+
+    public String get(String name) {
+        return null;
+    }
+
+    public Boolean hasEntry(String phoneNumber) {
+        return null;
+    }
+
+    public void remove(String name) {
+
+    }
+
+    public List<String> getAllContactNames() {
+        return null;
+    }
+
+    public String reverseLookup(String phoneNumber) {
+        return null;
+    }
+}
