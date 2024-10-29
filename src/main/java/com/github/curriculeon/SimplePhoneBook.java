3425415832f7337ea4a2f96a45af46520d9b562e
Mohammed N <mnaser11218@gmail.com>
2024-10-29 13:52:24 -0400

    completed simplePhonebook test

--- src/main/java/com/github/curriculeon/SimplePhoneBook.java
+++ src/main/java/com/github/curriculeon/SimplePhoneBook.java
@@ -1,32 +1,45 @@
 package com.github.curriculeon;
 
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by leon on 12/16/2019.
  */
 public class SimplePhoneBook {
+    private Map<String, String> phonebook = new LinkedHashMap<>();
     public void add(String name, String phoneNumberToAdd) {
+        this.phonebook.put(name, phoneNumberToAdd);
 
     }
 
     public String get(String name) {
-        return null;
+        return this.phonebook.get(name);
     }
 
     public Boolean hasEntry(String phoneNumber) {
-        return null;
+        return this.phonebook.containsValue(phoneNumber) ;
     }
 
     public void remove(String name) {
-
+        this.phonebook.remove(name);
     }
 
     public List<String> getAllContactNames() {
-        return null;
+        List<String>results = new ArrayList<>();
+        for(Map.Entry<String, String> number : this.phonebook.entrySet()){
+          results.add(number.getKey());
+        }
+        return results;
     }
 
     public String reverseLookup(String phoneNumber) {
+
+        for (Map.Entry<String, String> num : this.phonebook.entrySet()) {
+            if (num.getValue().equals(phoneNumber)) {
+                return num.getKey();
+            }
+        }
         return null;
     }
+
 }
